79e349451d28a696aca19d30b80e78534343dcdb
Matthew <defender_0000@mail.ru>
2024-09-21 18:31:46 +0300

    Creating data models

---
+++ spring_application/src/main/java/ru/matthew/controller/PlaceController.java
@@ -0,0 +1,10 @@
+package ru.matthew.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/places/categories")
+public class PlaceController {
+
+}
---
+++ spring_application/src/main/java/ru/matthew/model/Location.java
@@ -0,0 +1,13 @@
+package ru.matthew.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Location {
+    private String slug;
+    private String name;
+}
