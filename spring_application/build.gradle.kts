45907ad58e68efab4c8e6289ea0fe88d0b8f2cec
Matthew <defender_0000@mail.ru>
2024-09-30 22:15:51 +0300

    Adding the necessary dependencies, configuring JaCoCo

--- spring_application/build.gradle.kts
+++ spring_application/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     id("org.springframework.boot") version "3.3.4"
     id("io.spring.dependency-management") version "1.1.6"
+    id("jacoco")
 }
 
 group = "ru.matthew"
@@ -16,8 +17,30 @@ dependencies {
     implementation(libs.spring.boot)
     compileOnly(libs.lombok)
     annotationProcessor(libs.lombok)
+    testImplementation(libs.mockito)
+    testImplementation(libs.junit.api)
+    testRuntimeOnly(libs.junit.engine)
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+jacoco {
+    toolVersion = "0.8.8"
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
+        html.required.set(true)
+    }
+
+    afterEvaluate {
+        classDirectories.setFrom(files(
+            fileTree("${layout.buildDirectory}/classes/java/main").exclude("/dto/")
+        ))
+    }
 }
